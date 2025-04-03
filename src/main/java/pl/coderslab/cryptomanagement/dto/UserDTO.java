8a3cfb9e9465229916c8f0b7ff2d97f017e02f2f
eeee-dot <ejamroz@visa.com>
2025-04-03 09:16:32 +0200

    UserDTO

--- src/main/java/pl/coderslab/cryptomanagement/dto/UserDTO.java
+++ src/main/java/pl/coderslab/cryptomanagement/dto/UserDTO.java
@@ -1,4 +1,18 @@
 package pl.coderslab.cryptomanagement.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+import java.time.LocalDateTime;
+
+@Data
 public class UserDTO {
+    private Long userId;
+    @Size(min = 5, max = 50)
+    private String username;
+    @Email
+    private String email;
+    @Size(min = 8)
+    private String passwordHash;
+    private LocalDateTime created;
 }
