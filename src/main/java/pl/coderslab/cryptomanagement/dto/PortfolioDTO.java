d2459614bae15e821ccb81c173c661567c66f74f
eeee-dot <ejamroz@visa.com>
2025-04-03 08:51:38 +0200

    PortfolioDTO

--- src/main/java/pl/coderslab/cryptomanagement/dto/PortfolioDTO.java
+++ src/main/java/pl/coderslab/cryptomanagement/dto/PortfolioDTO.java
@@ -1,4 +1,25 @@
 package pl.coderslab.cryptomanagement.dto;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+import pl.coderslab.cryptomanagement.entity.User;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Data
 public class PortfolioDTO {
+
+    @DecimalMin(value = "0.0", inclusive = false)
+    private Long portfolioId;
+    private BigDecimal totalValue;
+    private LocalDate createdAt;
+    private LocalDateTime updatedAt;
+    private Long userId;
 }
