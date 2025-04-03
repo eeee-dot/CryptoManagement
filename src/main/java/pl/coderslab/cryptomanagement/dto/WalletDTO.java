0073a0c27de952bb4eb9dfd2fd763d7008f84459
eeee-dot <ejamroz@visa.com>
2025-04-03 14:23:45 +0200

    WalletService update function with user field

--- src/main/java/pl/coderslab/cryptomanagement/dto/WalletDTO.java
+++ src/main/java/pl/coderslab/cryptomanagement/dto/WalletDTO.java
@@ -10,6 +10,7 @@ public class WalletDTO {
     private Long walletId;
     private String name;
     private LocalDate createdAt;
+    private Long userId;
 
     @DecimalMin(value = "0.0", inclusive = false)
     private BigDecimal balance;
--- src/main/java/pl/coderslab/cryptomanagement/service/WalletService.java
+++ src/main/java/pl/coderslab/cryptomanagement/service/WalletService.java
@@ -4,20 +4,22 @@ import jakarta.validation.Validator;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import pl.coderslab.cryptomanagement.dto.WalletDTO;
-import pl.coderslab.cryptomanagement.entity.Coin;
 import pl.coderslab.cryptomanagement.entity.User;
 import pl.coderslab.cryptomanagement.entity.Wallet;
 import pl.coderslab.cryptomanagement.exception.ResourceNotFoundException;
 import pl.coderslab.cryptomanagement.generic.GenericService;
+import pl.coderslab.cryptomanagement.repository.UserRepository;
 import pl.coderslab.cryptomanagement.repository.WalletRepository;
 
 @Service
 public class WalletService extends GenericService<Wallet> {
     private final WalletRepository walletRepository;
-    
-    public WalletService(WalletRepository walletRepository, Validator validator) {
+    private final UserRepository userRepository;
+
+    public WalletService(WalletRepository walletRepository, Validator validator, UserRepository userRepository) {
         super(walletRepository, validator);
         this.walletRepository = walletRepository;
+        this.userRepository = userRepository;
     }
     
     public ResponseEntity<Wallet> update(Long id, WalletDTO walletDTO) {
@@ -28,6 +30,13 @@ public class WalletService extends GenericService<Wallet> {
                     }
                     if (walletDTO.getBalance() != null) {
                         walletToUpdate.setBalance(walletDTO.getBalance());
+                    }
+                    if (walletDTO.getUserId() != null) {
+                        User user = userRepository
+                                .findById(walletDTO.getUserId())
+                                .orElseThrow(() -> new ResourceNotFoundException(walletDTO.getUserId()));
+
+                        walletToUpdate.setUser(user);
                     }
 
                     return ResponseEntity.ok(walletRepository.save(walletToUpdate));
