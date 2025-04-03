c3ba02ac6e1e448c7f6536f6b13c2820c21e9f35
eeee-dot <ejamroz@visa.com>
2025-04-03 12:32:20 +0200

    Update function in PortfolioServices

---
+++ src/main/java/pl/coderslab/cryptomanagement/service/PortfolioService.java
@@ -0,0 +1,47 @@
+package pl.coderslab.cryptomanagement.service;
+
+import jakarta.validation.Validator;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import pl.coderslab.cryptomanagement.dto.PortfolioDTO;
+import pl.coderslab.cryptomanagement.entity.Portfolio;
+import pl.coderslab.cryptomanagement.entity.User;
+import pl.coderslab.cryptomanagement.exception.ResourceNotFoundException;
+import pl.coderslab.cryptomanagement.generic.GenericService;
+import pl.coderslab.cryptomanagement.repository.PortfolioRepository;
+import pl.coderslab.cryptomanagement.repository.UserRepository;
+
+
+@Service
+public class PortfolioService extends GenericService<Portfolio> {
+    private final PortfolioRepository portfolioRepository;
+    private final UserRepository userRepository;
+
+    public PortfolioService(PortfolioRepository portfolioRepository, Validator validator, UserRepository userRepository) {
+        super(portfolioRepository, validator);
+        this.portfolioRepository = portfolioRepository;
+        this.userRepository = userRepository;
+    }
+
+    public ResponseEntity<Portfolio> update(Long id, PortfolioDTO portfolioDTO) {
+        return portfolioRepository.findById(id)
+                .map(portfolioToUpdate -> {
+                    if (portfolioDTO.getCreatedAt() != null) {
+                        portfolioToUpdate.setCreatedAt(portfolioDTO.getCreatedAt());
+                    }
+                    if (portfolioDTO.getTotalValue() != null) {
+                        portfolioToUpdate.setTotalValue(portfolioDTO.getTotalValue());
+                    }
+                    if (portfolioDTO.getUpdatedAt() != null) {
+                        portfolioToUpdate.setUpdatedAt(portfolioDTO.getUpdatedAt());
+                    }
+                    if (portfolioDTO.getUserId() != null) {
+                        User user = userRepository.findById(id).orElseThrow(()->new ResourceNotFoundException(id));
+                        portfolioToUpdate.setUser(user);
+                    }
+                    return ResponseEntity.ok(portfolioRepository.save(portfolioToUpdate));
+                })
+                .orElseThrow(() -> new ResourceNotFoundException(id));
+    }
+    
+}
