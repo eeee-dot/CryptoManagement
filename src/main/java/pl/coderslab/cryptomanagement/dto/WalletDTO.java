a049a89c4b154e3e7fe40ecee72dfd67f726ea01
eeee-dot <ejamroz@visa.com>
2025-04-03 09:29:41 +0200

    WalletDTO

--- src/main/java/pl/coderslab/cryptomanagement/dto/WalletDTO.java
+++ src/main/java/pl/coderslab/cryptomanagement/dto/WalletDTO.java
@@ -1,4 +1,23 @@
 package pl.coderslab.cryptomanagement.dto;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Data
 public class WalletDTO {
+    private Long walletId;
+    private String name;
+    private LocalDate createdAt;
+
+    @DecimalMin(value = "0.0", inclusive = false)
+    private BigDecimal balance;
+
 }
