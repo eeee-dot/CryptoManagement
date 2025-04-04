aa541bd4508dbd881590f824d0dfdcfa5a9618c8
eeee-dot <ejamroz@visa.com>
2025-04-04 12:59:08 +0200

    UserController

---
+++ src/main/java/pl/coderslab/cryptomanagement/controller/UserController.java
@@ -0,0 +1,27 @@
+package pl.coderslab.cryptomanagement.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PatchMapping;
+import pl.coderslab.cryptomanagement.dto.UserDTO;
+import pl.coderslab.cryptomanagement.entity.User;
+import pl.coderslab.cryptomanagement.generic.GenericController;
+import pl.coderslab.cryptomanagement.service.UserService;
+
+@Controller
+public class UserController extends GenericController<User> {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        super(userService, User.class);
+        this.userService = userService;
+    }
+
+    @PatchMapping("/user")
+    public ResponseEntity<User> updateUser() {
+        userService.update(1L, new UserDTO());
+        return ResponseEntity.ok().build();
+    }
+
+
+}
