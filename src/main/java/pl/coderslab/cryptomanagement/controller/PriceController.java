14740cbf838e754e225d27dc0237d5428d2e0eb6
eeee-dot <ejamroz@visa.com>
2025-04-04 12:58:21 +0200

    Added PriceController

---
+++ src/main/java/pl/coderslab/cryptomanagement/controller/PriceController.java
@@ -0,0 +1,27 @@
+package pl.coderslab.cryptomanagement.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PatchMapping;
+import pl.coderslab.cryptomanagement.dto.PriceDTO;
+import pl.coderslab.cryptomanagement.entity.Price;
+import pl.coderslab.cryptomanagement.generic.GenericController;
+import pl.coderslab.cryptomanagement.service.PriceService;
+
+@Controller
+public class PriceController extends GenericController<Price> {
+    private final PriceService priceService;
+
+    public PriceController(PriceService priceService) {
+        super(priceService, Price.class);
+        this.priceService = priceService;
+    }
+
+    @PatchMapping("/price")
+    public ResponseEntity<Price> updatePrice() {
+        priceService.update(1L, new PriceDTO());
+        return ResponseEntity.ok().build();
+    }
+
+
+}
