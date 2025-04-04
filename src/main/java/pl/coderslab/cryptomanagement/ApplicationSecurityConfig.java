068b070248e6712d6a617d249f02a1a49558c7bb
eeee-dot <ejamroz@visa.com>
2025-04-04 16:40:01 +0200

    Authentication logic

---
+++ src/main/java/pl/coderslab/cryptomanagement/ApplicationSecurityConfig.java
@@ -0,0 +1,55 @@
+package pl.coderslab.cryptomanagement;
+
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import pl.coderslab.cryptomanagement.service.UserService;
+
+@Configuration
+@AllArgsConstructor
+public class ApplicationSecurityConfig {
+    private UserService userService;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+
+        http.authorizeHttpRequests(
+                        authorize -> authorize
+                                .requestMatchers("/public/**", "/resources/**", "/fonts/**", "/img/**")
+                                .permitAll()
+                                .anyRequest()
+                                .authenticated()
+                ).formLogin(
+                        form -> form
+                                .loginPage("/login")
+                                .permitAll())
+                .logout(
+                        logout -> logout
+                                .logoutUrl("/logout")
+                                .permitAll());
+
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setUserDetailsService(userService);
+        provider.setPasswordEncoder(passwordEncoder());
+        return provider;
+    }
+
+}
