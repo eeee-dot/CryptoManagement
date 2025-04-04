539ec2d4bd7bbf4c9905cbf51101d861569034c0
eeee-dot <ejamroz@visa.com>
2025-04-04 15:59:42 +0200

    Login by spring security

--- src/main/java/pl/coderslab/cryptomanagement/repository/UserRepository.java
+++ src/main/java/pl/coderslab/cryptomanagement/repository/UserRepository.java
@@ -3,4 +3,6 @@ package pl.coderslab.cryptomanagement.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import pl.coderslab.cryptomanagement.entity.User;
 
-public interface UserRepository extends JpaRepository<User, Long> {}
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+}
--- src/main/java/pl/coderslab/cryptomanagement/service/UserService.java
+++ src/main/java/pl/coderslab/cryptomanagement/service/UserService.java
@@ -2,15 +2,18 @@ package pl.coderslab.cryptomanagement.service;
 
 import jakarta.validation.Validator;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import pl.coderslab.cryptomanagement.dto.UserDTO;
 import pl.coderslab.cryptomanagement.entity.User;
 import pl.coderslab.cryptomanagement.exception.ResourceNotFoundException;
 import pl.coderslab.cryptomanagement.generic.GenericService;
 import pl.coderslab.cryptomanagement.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 
 @Service
-public class UserService extends GenericService<User> {
+public class UserService extends GenericService<User> implements UserDetailsService {
     private final UserRepository userRepository;
     
     public UserService (UserRepository userRepository, Validator validator) {
@@ -34,5 +37,11 @@ public class UserService extends GenericService<User> {
                     return ResponseEntity.ok(userRepository.save(userToUpdate));
                 })
                 .orElseThrow(() -> new ResourceNotFoundException(id));
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        return null;
     }
 }
