68b4b48c438c3066fbac2a9c9ae03122dc0aab82
eeee-dot <ejamroz@visa.com>
2025-04-04 12:59:52 +0200

    WalletController added

---
+++ src/main/java/pl/coderslab/cryptomanagement/controller/WalletController.java
@@ -0,0 +1,27 @@
+package pl.coderslab.cryptomanagement.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PatchMapping;
+import pl.coderslab.cryptomanagement.dto.WalletDTO;
+import pl.coderslab.cryptomanagement.entity.Wallet;
+import pl.coderslab.cryptomanagement.generic.GenericController;
+import pl.coderslab.cryptomanagement.service.WalletService;
+
+@Controller
+public class WalletController extends GenericController<Wallet> {
+    private final WalletService walletService;
+
+    public WalletController(WalletService walletService) {
+        super(walletService, Wallet.class);
+        this.walletService = walletService;
+    }
+
+    @PatchMapping("/wallet")
+    public ResponseEntity<Wallet> updateWallet() {
+        walletService.update(1L, new WalletDTO());
+        return ResponseEntity.ok().build();
+    }
+
+
+}
