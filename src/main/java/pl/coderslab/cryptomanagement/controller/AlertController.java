88d45160427fa66303a6284c3f1bfce68c1303e8
eeee-dot <ejamroz@visa.com>
2025-04-04 12:55:02 +0200

    AlertController

---
+++ src/main/java/pl/coderslab/cryptomanagement/controller/AlertController.java
@@ -0,0 +1,27 @@
+package pl.coderslab.cryptomanagement.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PatchMapping;
+import pl.coderslab.cryptomanagement.dto.AlertDTO;
+import pl.coderslab.cryptomanagement.entity.Alert;
+import pl.coderslab.cryptomanagement.generic.GenericController;
+import pl.coderslab.cryptomanagement.service.AlertService;
+
+@Controller
+public class AlertController extends GenericController<Alert> {
+    private final AlertService alertService;
+
+    public AlertController(AlertService alertService) {
+        super(alertService, Alert.class);
+        this.alertService = alertService;
+    }
+
+    @PatchMapping("alert")
+    public ResponseEntity<Alert> updateAlert() {
+        alertService.update(1L, new AlertDTO());
+        return ResponseEntity.ok().build();
+    }
+
+
+}
