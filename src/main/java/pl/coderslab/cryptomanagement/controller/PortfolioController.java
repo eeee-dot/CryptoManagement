610f86207f7f97bc8188b84ab07f411370fde5aa
eeee-dot <ejamroz@visa.com>
2025-04-04 12:57:32 +0200

    Added PortfolioController

---
+++ src/main/java/pl/coderslab/cryptomanagement/controller/PortfolioController.java
@@ -0,0 +1,27 @@
+package pl.coderslab.cryptomanagement.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PatchMapping;
+import pl.coderslab.cryptomanagement.dto.PortfolioDTO;
+import pl.coderslab.cryptomanagement.entity.Portfolio;
+import pl.coderslab.cryptomanagement.generic.GenericController;
+import pl.coderslab.cryptomanagement.service.PortfolioService;
+
+@Controller
+public class PortfolioController extends GenericController<Portfolio> {
+    private final PortfolioService portfolioService;
+
+    public PortfolioController(PortfolioService portfolioService) {
+        super(portfolioService, Portfolio.class);
+        this.portfolioService = portfolioService;
+    }
+
+    @PatchMapping("/portfolio")
+    public ResponseEntity<Portfolio> updatePortfolio() {
+        portfolioService.update(1L, new PortfolioDTO());
+        return ResponseEntity.ok().build();
+    }
+
+
+}
