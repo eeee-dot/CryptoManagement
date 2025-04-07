a347225b43b5337fa1cda274230c38bb6d767e3f
eeee-dot <ejamroz@visa.com>
2025-04-07 13:33:01 +0200

    Listing coins from database

--- src/main/java/pl/coderslab/cryptomanagement/controller/CoinController.java
+++ src/main/java/pl/coderslab/cryptomanagement/controller/CoinController.java
@@ -1,12 +1,14 @@
 package pl.coderslab.cryptomanagement.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import pl.coderslab.cryptomanagement.entity.Coin;
 import pl.coderslab.cryptomanagement.generic.GenericController;
 import pl.coderslab.cryptomanagement.service.CoinService;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/coin")
@@ -19,7 +21,9 @@ public class CoinController extends GenericController<Coin> {
     }
 
     @GetMapping("/coin")
-    public String getCoins() {
+    public String getCoins(Model model) {
+        List<Coin> coins = coinService.getAll().getBody();
+        model.addAttribute("coins", coins);
         return "coins";
     }
 
