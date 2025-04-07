0d0af9df8941d22feedfa07f3613e7d519411f42
eeee-dot <ejamroz@visa.com>
2025-04-07 14:59:58 +0200

    Wallets page

--- src/main/java/pl/coderslab/cryptomanagement/controller/WalletController.java
+++ src/main/java/pl/coderslab/cryptomanagement/controller/WalletController.java
@@ -1,13 +1,14 @@
 package pl.coderslab.cryptomanagement.controller;
 
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import pl.coderslab.cryptomanagement.dto.WalletDTO;
 import pl.coderslab.cryptomanagement.entity.Wallet;
 import pl.coderslab.cryptomanagement.generic.GenericController;
 import pl.coderslab.cryptomanagement.service.WalletService;
+import org.springframework.ui.Model;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/wallet")
@@ -19,10 +20,11 @@ public class WalletController extends GenericController<Wallet> {
         this.walletService = walletService;
     }
 
-    @PatchMapping("/wallet")
-    public ResponseEntity<Wallet> updateWallet() {
-        walletService.update(1L, new WalletDTO());
-        return ResponseEntity.ok().build();
+    @GetMapping()
+    public String updateWallet(Model model) {
+        List<Wallet> wallets = walletService.getAll().getBody();
+        model.addAttribute("wallets", wallets);
+        return "wallets";
     }
 
 
