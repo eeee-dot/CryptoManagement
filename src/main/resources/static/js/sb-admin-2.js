7dfcc04922aee23750c22425731c517177f07c10
eeee-dot <ejamroz@visa.com>
2025-04-09 14:14:01 +0200

    Time counting index

--- src/main/resources/static/js/sb-admin-2.js
+++ src/main/resources/static/js/sb-admin-2.js
@@ -20,6 +20,21 @@ document.getElementById("deleteButton").addEventListener("click", function(event
   deleteWallet(this);
 });
 
+function time() {
+  const now = new Date();
+  const midnight = new Date(now.getFullYear(), now.getMonth(), now.getDate());
+  const difference = now - midnight;
+  const hours = Math.floor(difference / (1000 * 60 * 60));
+  const minutes = Math.floor(difference % (1000 * 60 * 60) / (1000 * 60));
+
+  let timeElapsed = `${hours}:${minutes}`;
+
+  document.getElementById("clock").innerText = `${timeElapsed}`;
+}
+
+setInterval(time, 60000);
+time();
+
   // Toggle the side navigation
   $("#sidebarToggle, #sidebarToggleTop").on('click', function(e) {
     $("body").toggleClass("sidebar-toggled");
