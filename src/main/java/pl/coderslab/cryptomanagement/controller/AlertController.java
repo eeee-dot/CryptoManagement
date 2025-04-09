0c4b375137d974b074581dd7ae20016cdab6a023
eeee-dot <ejamroz@visa.com>
2025-04-09 11:59:33 +0200

    User data from AuthenticationPrincipal

--- src/main/java/pl/coderslab/cryptomanagement/controller/AlertController.java
+++ src/main/java/pl/coderslab/cryptomanagement/controller/AlertController.java
@@ -1,13 +1,14 @@
 package pl.coderslab.cryptomanagement.controller;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import pl.coderslab.cryptomanagement.entity.Alert;
 import pl.coderslab.cryptomanagement.entity.Coin;
 import pl.coderslab.cryptomanagement.entity.User;
-import pl.coderslab.cryptomanagement.generic.GenericController;
 import pl.coderslab.cryptomanagement.service.AlertService;
 import pl.coderslab.cryptomanagement.repository.UserRepository;
 import pl.coderslab.cryptomanagement.service.CoinService;
@@ -19,30 +20,23 @@ import java.util.Optional;
 
 @Controller
 @RequestMapping("/alert")
-public class AlertController extends GenericController<Alert> {
+public class AlertController{
     private final AlertService alertService;
-    private final UserRepository userRepository;
     private final CoinService coinService;
     private final UserService userService;
 
     public AlertController(AlertService alertService, UserRepository userRepository, CoinService coinService, UserService userService) {
-        super(alertService, Alert.class);
         this.alertService = alertService;
-        this.userRepository = userRepository;
         this.coinService = coinService;
         this.userService = userService;
     }
 
     @GetMapping()
-    public String getAlerts(Model model) {
-        String currentPrincipalName = SecurityContextHolder.getContext().getAuthentication().getName();
-        Optional<User> user = userRepository.findByUsername(currentPrincipalName);
+    public String getAlerts(Model model, @AuthenticationPrincipal UserDetails userDetails) {
+        String username = userDetails.getUsername();
+        User user = userService.loadByUsername(username).getBody();
 
-        if (user.isEmpty()) {
-            return "404";
-        }
-
-        List<Alert> alerts = alertService.loadAlertsByUser(user.get()).getBody();
+        List<Alert> alerts = alertService.loadAlertsByUser(user).getBody();
         model.addAttribute("alerts", alerts);
         return "alerts";
     }
