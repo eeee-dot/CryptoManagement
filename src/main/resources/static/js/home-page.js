7c02767f46e85fbe3122b19ffb697183e9e21e29
eeee-dot <ejamroz@visa.com>
2025-04-10 11:07:28 +0200

    time since last update formated

--- src/main/resources/static/js/home-page.js
+++ src/main/resources/static/js/home-page.js
@@ -4,8 +4,19 @@ function time() {
     const difference = now - midnight;
     const hours = Math.floor(difference / (1000 * 60 * 60));
     const minutes = Math.floor(difference % (1000 * 60 * 60) / (1000 * 60));
-
-    let timeElapsed = `${hours}h:${minutes}m`;
+    let hourFormat;
+    let minuteFormat;
+    if (minutes < 10) {
+        minuteFormat = `0${minutes}`
+    } else {
+        minuteFormat= `${minutes}`;
+    }
+    if (hours < 10) {
+        hourFormat = `0${hours}`
+    } else {
+        hourFormat = `${hours}`
+    }
+    let timeElapsed = `${hourFormat}h:${minuteFormat}m`;
 
     document.getElementById("clock").innerText = `${timeElapsed}`;
 }
